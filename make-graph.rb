5358437114fb20ccc24fed5e937f2d4787b47fae
Will Madden <w.a.madden@gmail.com>
2015-12-05 12:52:12 +0100

    (sophie) Add some real data

--- make-graph.rb
+++ make-graph.rb
@@ -6,11 +6,28 @@ require 'ruby-graphviz'
 g = GraphViz.new( :G, :type => :digraph )
 
 # Create two nodes
-hello = g.add_nodes( "Hello" )
-world = g.add_nodes( "World" )
+one1 = g.add_nodes( "1.1" )
+one2 = g.add_nodes( "1.2" )
+one3 = g.add_nodes( "1.3" )
+
+two1 = g.add_nodes( "2.1" )
+two2 = g.add_nodes( "2.2" )
+two3 = g.add_nodes( "2.3" )
+
+three1 = g.add_nodes( "3.1" )
+three2 = g.add_nodes( "3.2" )
+three3 = g.add_nodes( "3.3" )
 
 # Create an edge between the two nodes
-g.add_edges( hello, world )
+g.add_edges( one1, one2 )
+g.add_edges( one1, one3 )
+g.add_edges( one2, two1 )
+g.add_edges( one3, two1 )
+g.add_edges( one3, three1 )
+g.add_edges( one3, three2 )
+g.add_edges( one3, three3 )
+g.add_edges( two1, two2 )
+g.add_edges( two1, two3 )
 
 # Generate output image
 g.output( :png => "hello_world.png" )
