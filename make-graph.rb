e1bf70d8ced8f6d05ccc1f24a67a11a0635f7fc4
Will Madden <w.a.madden@gmail.com>
2015-12-05 12:40:08 +0100

    Initial commit

---
+++ make-graph.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'ruby-graphviz'
+
+# Create a new graph
+g = GraphViz.new( :G, :type => :digraph )
+
+# Create two nodes
+hello = g.add_nodes( "Hello" )
+world = g.add_nodes( "World" )
+
+# Create an edge between the two nodes
+g.add_edges( hello, world )
+
+# Generate output image
+g.output( :png => "hello_world.png" )
