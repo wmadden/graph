bab3feab53c81dcfe0bfedc265d3820b94d79dea
Will Madden <w.a.madden@gmail.com>
2015-12-06 12:26:20 +0100

    Update `make-graph` script

--- make-graph.rb
+++ make-graph.rb
@@ -2,7 +2,7 @@
 
 require "./lib/graph"
 
-g = Graph.new({
+g = GraphBuilder.from_hash({
   "Marktanalyse" => { duration: 3, dependencies: %w(Zielgruppe Spielmechanik) },
   "Zielgruppe" => { duration: 2, dependencies: %w(Story) },
   "Spielmechanik" => { duration: 5, dependencies: %w(Story MovementSystem AttackSystem ScoreSystem) },
@@ -12,43 +12,43 @@ g = Graph.new({
   "EnemyDesign" => { duration: 10, dependencies: %w(M1) },
   "EnvironmentDesign" => { duration: 10, dependencies: %w(M1) },
   "LevelDesign" => { duration: 10, dependencies: %w(M1) },
-  "M1" => { duration: 0, dependencies: %w(Enemy3D LevelDeko Boss3D EnvironmentEffekte EnemySounds GegnerKlassen Boss StartMenu OptionsMenu StageSelectMenu InGameHUD GUISounds EnvironmentSounds Musik Level1bis5 BossLevel Player3D PlayerEffekte EnemyEffekte StageSelectMenu PlayerSkripte PlayerSounds) },
+  "M1" => { duration: 0, milestone: true, dependencies: %w(Enemy3D LevelDeko Boss3D EnvironmentEffekte EnemySounds GegnerKlassen Boss StartMenu OptionsMenu InGameHUD GUISounds EnvironmentSounds Musik Level1bis5 BossLevel Player3D PlayerEffekte EnemyEffekte StageSelectMenu PlayerSkripte PlayerSounds) },
 
   "MovementSystem" => { duration: 5, dependencies: %w(M2) },
   "AttackSystem" => { duration: 5, dependencies: %w(M2) },
   "ScoreSystem" => { duration: 5, dependencies: %w(M2) },
-  "M2" => { duration: 0, dependencies: %w(PlayerEffekte EnemyEffekte EnvironmentEffekte Level1bis5 BossLevel PlayerSkripte GegnerKlassen Boss SpawnSystem) },
+  "M2" => { duration: 0, milestone: true, dependencies: %w(PlayerEffekte EnemyEffekte EnvironmentEffekte Level1bis5 BossLevel PlayerSkripte GegnerKlassen Boss SpawnSystem) },
 
   "Player3D" => { duration: 10, dependencies: %w(M3 PlayerRigAnimation) },
   "PlayerRigAnimation" => { duration: 10, dependencies: %w(M3 EnemyEffekte) },
-  "M3" => { duration: 0, dependencies: %w(PlayerEffekte) },
+  "M3" => { duration: 0, milestone: true, dependencies: %w(PlayerEffekte) },
 
   "Enemy3D" => { duration: 15, dependencies: %w(M4 EnemyRigAnimation) },
   "EnemyRigAnimation" => { duration: 20, dependencies: %w(M4) },
-  "M4" => { duration: 0, dependencies: %w(EnemyEffekte) },
+  "M4" => { duration: 0, milestone: true, dependencies: %w(EnemyEffekte) },
 
   "LevelDeko" => { duration: 20, dependencies: %w(M5) },
   "Boss3D" => { duration: 10, dependencies: %w(M5 BossRigAnimation) },
   "BossRigAnimation" => { duration: 10, dependencies: %w(M5) },
-  "M5" => { duration: 0, dependencies: %w(EarlyAccess) },
+  "M5" => { duration: 0, milestone: true, dependencies: %w(EarlyAccess) },
 
   "PlayerEffekte" => { duration: 5, dependencies: %w(M6) },
   "EnemyEffekte" => { duration: 5, dependencies: %w(M6) },
   "EnvironmentEffekte" => { duration: 10, dependencies: %w(M6) },
-  "M6" => { duration: 0, dependencies: %w(EarlyAccess) },
+  "M6" => { duration: 0, milestone: true, dependencies: %w(EarlyAccess) },
 
   "StartMenu" => { duration: 5, dependencies: %w(M7) },
   "OptionsMenu" => { duration: 5, dependencies: %w(M7) },
   "StageSelectMenu" => { duration: 5, dependencies: %w(M7) },
   "InGameHUD" => { duration: 10, dependencies: %w(M7) },
-  "M7" => { duration: 0, dependencies: %w(EarlyAccess) },
+  "M7" => { duration: 0, milestone: true, dependencies: %w(EarlyAccess) },
 
   "GUISounds" => { duration: 10, dependencies: %w(M8) },
   "PlayerSounds" => { duration: 10, dependencies: %w(M8) },
   "EnemySounds" => { duration: 15, dependencies: %w(M8) },
   "EnvironmentSounds" => { duration: 15, dependencies: %w(M8) },
   "Musik" => { duration: 30, dependencies: %w(M8) },
-  "M8" => { duration: 0, dependencies: %w(EarlyAccess) },
+  "M8" => { duration: 0, milestone: true, dependencies: %w(EarlyAccess) },
 
   "Level1bis5" => { duration: 20, dependencies: %w(M9) },
   "BossLevel" => { duration: 5, dependencies: %w(M9) },
@@ -56,19 +56,9 @@ g = Graph.new({
   "GegnerKlassen" => { duration: 20, dependencies: %w(M9) },
   "Boss" => { duration: 10, dependencies: %w(M9) },
   "SpawnSystem" => { duration: 10, dependencies: %w(M9) },
-  "M9" => { duration: 0, dependencies: %w(EarlyAccess) },
-  "EarlyAccess" => { duration: 0, dependencies: [] },
+  "M9" => { duration: 0, milestone: true, dependencies: %w(EarlyAccess) },
+  "EarlyAccess" => { duration: 0, milestone: true, dependencies: [] },
 })
 
-g.node("M1")[:shape] = "diamond"
-g.node("M2")[:shape] = "diamond"
-g.node("M3")[:shape] = "diamond"
-g.node("M4")[:shape] = "diamond"
-g.node("M5")[:shape] = "diamond"
-g.node("M6")[:shape] = "diamond"
-g.node("M7")[:shape] = "diamond"
-g.node("M8")[:shape] = "diamond"
-g.node("M9")[:shape] = "diamond"
-
 # Generate output image
-g.draw(:pdf, "graph")
+GraphRenderer.draw(g, :pdf, "graph")
